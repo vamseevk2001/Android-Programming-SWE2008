34b9ed7fb9255912c9a43f6ccd492019d4d391e1
vamseevk <vamseevk2001@gmail.cmo>
2021-08-30 21:07:06 +0530

    added an app which uses services for user authentication

---
+++ user_authentication_using_services/app/src/main/java/vamsee/application/user_authentication_using_services/UserAuthentication.java
@@ -0,0 +1,51 @@
+package vamsee.application.user_authentication_using_services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import java.net.URISyntaxException;
+
+public class UserAuthentication extends Service {
+    public UserAuthentication() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+
+            String username = intent.getExtras().getString("username");
+            String password = intent.getExtras().getString("password");
+
+//            Toast.makeText(getApplicationContext(), username + password, Toast.LENGTH_SHORT).show();
+
+            //username = vamseevk2001
+            // Password = vamseevk
+
+            if(username.equals("vamseevk2001") && password.equals("vamseevk")){
+                Toast.makeText(getApplicationContext(), "Logged in successfully..", Toast.LENGTH_SHORT).show();
+                Intent next = new Intent(getApplicationContext(), Dashboard.class);
+                next.putExtra("name", username);
+                next.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(next);
+            }
+            else {
+                Toast.makeText(getApplicationContext(), "Please check your credentials...", Toast.LENGTH_SHORT).show();
+            }
+
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
